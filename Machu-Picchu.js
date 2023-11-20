b80ce72e969606d9ed23f7afb4e0086abc03cd91
Sade Adesida <150923026+Sade090@users.noreply.github.com>
2023-11-20 17:31:50 +0000

    Create Machu-Picchu.js

---
+++ Machu-Picchu.js
@@ -0,0 +1,59 @@
+<section className={styles['section01']}>
+        <h2 className={styles['text02']}>
+          Our doctors and therapists are here, 24/7.
+        </h2>
+        <div className={styles['features']}>
+          <header
+            className={` ${projectStyles['feature']} ${projectStyles['feature-active']} ${styles['feature']} `}
+          >
+            <h3 className={styles['text03']}>Urgent Care</h3>
+            <p className={styles['text04']}>Doloremque laudantium</p>
+          </header>
+          <header
+            className={` ${projectStyles['feature']} ${styles['feature1']} `}
+          >
+            <h3 className={styles['text05']}>Chronic Care</h3>
+            <p className={styles['text06']}>Doloremque laudantium</p>
+          </header>
+          <header
+            className={` ${projectStyles['feature']} ${styles['feature2']} `}
+          >
+            <h3 className={styles['text07']}>Mental Health</h3>
+            <p className={styles['text08']}>Doloremque laudantium</p>
+          </header>
+        </div>
+        <div className={styles['note']}>
+          <div className={styles['content1']}>
+            <main className={styles['main1']}>
+              <h2 className={styles['heading01']}>
+                Accessing this Medicare benefit is easy
+              </h2>
+              <p className={styles['paragraph']}>
+                <span>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo inventore veritatis et quasi architecto
+                  beatae.
+                </span>
+                <br></br>
+                <br></br>
+                <span>
+                  Doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+                  illo inventore veritatis et quasi architecto beatae.
+                </span>
+                <br></br>
+              </p>
+            </main>
+            <div className={styles['explore-more']}>
+              <p className={styles['text14']}>Explore more -&gt;</p>
+            </div>
+          </div>
+          <div className={styles['image07']}>
+            <img
+              alt="image"
+              src="/group%201490-1200w.png"
+              className={styles['image08']}
+            />
+          </div>
+        </div>
+      </section>
